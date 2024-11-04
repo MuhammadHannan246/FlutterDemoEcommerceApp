095372faffb1890dbcbe41c1e34725c4c0b4a19c
Muhammad Hannan <sheikh.hannan06@gmail.com>
2024-11-04 12:16:30 +0500

    Create category_card_widget.dart

---
+++ lib/widgets/category_card_widget.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class CategoryCardWidget extends StatelessWidget {
+  final String imageUrl;
+  final String categoryName;
+
+  const CategoryCardWidget({
+    super.key,
+    required this.imageUrl,
+    required this.categoryName,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Stack(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8.0),
+            child: imageUrl.startsWith('http')
+                ? Image.network(
+                    imageUrl,
+                    fit: BoxFit.cover,
+                    errorBuilder: (context, error, stackTrace) {
+                      return Center(child: Text('Image not available'));
+                    },
+                  )
+                : Image.asset(
+                    imageUrl,
+                    fit: BoxFit.cover,
+                  ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.black.withOpacity(0.3),
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Text(
+                categoryName,
+                style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                      color: Colors.white,
+                      fontFamily: 'relative',
+                    ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
