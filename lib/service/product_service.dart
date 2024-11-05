36a862ac810ea00af9fb4c3637dff53f5fd84106
Muhammad Hannan <sheikh.hannan06@gmail.com>
2024-11-06 04:13:11 +0500

    Create product_service.dart

---
+++ lib/service/product_service.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:test/Model/product_data_model.dart';
+
+class ProductService {
+  Future<ProductDataModel?> fetchProductData() async {
+    try {
+      final response = await http.get(Uri.parse('https://dummyjson.com/products?limit=100'));
+
+      if (response.statusCode == 200) {
+        return ProductDataModel.fromJson(json.decode(response.body));
+      } else {
+        throw Exception('Failed to load products');
+      }
+    } catch (e) {
+      print('Error fetching data: $e');
+      return null;
+    }
+  }
+}
