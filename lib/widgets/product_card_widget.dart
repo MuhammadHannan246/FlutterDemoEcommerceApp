aa5407564ae649a9a769f77c2109e55ae3438087
Muhammad Hannan <sheikh.hannan06@gmail.com>
2024-11-06 04:12:59 +0500

    Update product_card_widget.dart

--- lib/widgets/product_card_widget.dart
+++ lib/widgets/product_card_widget.dart
@@ -2,15 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:test/colors/colors.dart';
 
-class ProductCard extends StatelessWidget {
+class ProductCardWidget extends StatelessWidget {
   final String productImage;
   final String productName;
   final double productRating;
   final double productPrice;
   final String productBrand;
   final String productCategory;
+  final VoidCallback onTap;
 
-  const ProductCard({
+  const ProductCardWidget({
     super.key,
     required this.productImage,
     required this.productName,
@@ -18,117 +19,121 @@ class ProductCard extends StatelessWidget {
     required this.productPrice,
     required this.productBrand,
     required this.productCategory,
+    required this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 0,
-      margin: const EdgeInsets.symmetric(vertical: 8.0),
-      shape: RoundedRectangleBorder(
-        borderRadius: const BorderRadius.only(
-          bottomLeft: Radius.circular(8.0),
-          bottomRight: Radius.circular(8.0),
-        ),
-        side: BorderSide(color: kGreyColor),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: ClipRRect(
-              borderRadius: const BorderRadius.only(
-                bottomRight: Radius.circular(8.0),
-                bottomLeft: Radius.circular(8.0),
-              ),
-              child: productImage.startsWith('http')
-                  ? Image.network(
-                      productImage,
-                      width: double.infinity,
-                      fit: BoxFit.cover,
-                    )
-                  : Image.asset(
-                      productImage,
-                      width: double.infinity,
-                      fit: BoxFit.cover,
-                    ),
-            ),
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        elevation: 0,
+        margin: const EdgeInsets.symmetric(vertical: 8.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: const BorderRadius.only(
+            bottomLeft: Radius.circular(8.0),
+            bottomRight: Radius.circular(8.0),
           ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Expanded(
-                      child: Text(
-                        productName,
-                        style: Theme.of(context).textTheme.bodyMedium,
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                    Flexible(
-                      child: Text(
-                        '\$$productPrice',
-                        style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                              fontWeight: FontWeight.bold,
-                            ),
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                  ],
+          side: BorderSide(color: kGreyColor),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  bottomRight: Radius.circular(8.0),
+                  bottomLeft: Radius.circular(8.0),
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 4.0),
-                  child: Row(
-                    children: [
-                      Text(
-                        '$productRating',
-                        style: Theme.of(context).textTheme.bodySmall,
+                child: productImage.startsWith('http')
+                    ? Image.network(
+                        productImage,
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                      )
+                    : Image.asset(
+                        productImage,
+                        width: double.infinity,
+                        fit: BoxFit.cover,
                       ),
-                      const SizedBox(width: 4),
-                      RatingBarIndicator(
-                        rating: productRating,
-                        itemBuilder: (context, index) => const Icon(
-                          Icons.star,
-                          color: Colors.amber,
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: Text(
+                          productName,
+                          style: Theme.of(context).textTheme.bodyMedium,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
                         ),
-                        itemCount: 5,
-                        itemSize: 16.0,
-                        direction: Axis.horizontal,
+                      ),
+                      Flexible(
+                        child: Text(
+                          '\$$productPrice',
+                          style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                fontWeight: FontWeight.bold,
+                              ),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
                       ),
                     ],
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 4.0),
-                  child: Text(
-                    productBrand,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 4.0),
+                    child: Row(
+                      children: [
+                        Text(
+                          '$productRating',
+                          style: Theme.of(context).textTheme.bodySmall,
+                        ),
+                        const SizedBox(width: 4),
+                        RatingBarIndicator(
+                          rating: productRating,
+                          itemBuilder: (context, index) => const Icon(
+                            Icons.star,
+                            color: Colors.amber,
+                          ),
+                          itemCount: 5,
+                          itemSize: 16.0,
+                          direction: Axis.horizontal,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 4.0),
+                    child: Text(
+                      productBrand,
+                      style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                            color: kGreyColor,
+                          ),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  Text(
+                    productCategory,
                     style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                          color: kGreyColor,
+                          color: kBlackColor,
                         ),
                     maxLines: 1,
                     overflow: TextOverflow.ellipsis,
                   ),
-                ),
-                Text(
-                  productCategory,
-                  style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                        color: kBlackColor,
-                      ),
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
