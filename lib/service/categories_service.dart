259397b5703814e7681ee1646b8fd5190a0d905d
Muhammad Hannan <sheikh.hannan06@gmail.com>
2024-11-06 04:13:19 +0500

    Create categories_service.dart

---
+++ lib/service/categories_service.dart
@@ -0,0 +1,21 @@
+import 'package:http/http.dart' as http;
+import 'package:test/Model/categories_data_model.dart';
+
+class CategoryService {
+  final String _baseUrl = 'https://dummyjson.com/products/categories';
+
+  Future<List<CategoriesDataModel>> fetchCategories() async {
+    try {
+      final response = await http.get(Uri.parse(_baseUrl));
+
+      if (response.statusCode == 200) {
+        return categoriesDataModelFromJson(response.body);
+      } else {
+        throw Exception('Failed to load categories');
+      }
+    } catch (e) {
+      print('Error fetching categories: $e');
+      return [];
+    }
+  }
+}
