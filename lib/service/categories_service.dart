5b8ee9ce8cd087e8a0da98534f0617b9a6d75922
Muhammad Hannan <sheikh.hannan06@gmail.com>
2024-11-06 05:05:39 +0500

    Update categories_service.dart

--- lib/service/categories_service.dart
+++ lib/service/categories_service.dart
@@ -1,5 +1,5 @@
 import 'package:http/http.dart' as http;
-import 'package:test/Model/categories_data_model.dart';
+import 'package:test/model/categories_data_model.dart';
 
 class CategoryService {
   final String _baseUrl = 'https://dummyjson.com/products/categories';
